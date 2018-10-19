974fc074dffe652d65bf0317518f285a1b934afe
Unknown <railken@protonmail.ch>
2018-10-19 13:10:32 +0200

    update configs

--- config/amethyst.nutrition.php
+++ config/amethyst.nutrition.php
@@ -3,90 +3,24 @@
 return [
     /*
     |--------------------------------------------------------------------------
-    | Managers
+    | Data
     |--------------------------------------------------------------------------
     |
-    | Here you can change the table name and the class components
-    | used by each manager.
+    | Here you can change the table name and the class components.
     |
     */
-    'managers' => [
+    'data' => [
         'nutrition' => [
-            /*
-            |--------------------------------------------------------------------------
-            | Table Name
-            |--------------------------------------------------------------------------
-            |
-            | This is the table name used while interacting with the database
-            |
-            */
-            'table' => 'amethyst_nutritions',
-
-            /*
-            |--------------------------------------------------------------------------
-            | Class Entity
-            |--------------------------------------------------------------------------
-            |
-            | The class of the model used by the manager.
-            | Change this if you have to add more relations or custom logic.
-            |
-            */
-            'model' => Railken\Amethyst\Models\Nutrition::class,
-
-            /*
-            |--------------------------------------------------------------------------
-            | Class Schema
-            |--------------------------------------------------------------------------
-            |
-            | The class of the schema used by the manager.
-            | Change this if you want to update the attributes.
-            */
-            'schema' => Railken\Amethyst\Schemas\NutritionSchema::class,
-
-            /*
-            |--------------------------------------------------------------------------
-            | Class Repository
-            |--------------------------------------------------------------------------
-            |
-            | The class of the repository used to perform all queries.
-            | Change this if you have to add more complex queries (e.g. ::findOneBy).
-            |
-            */
+            'table'      => 'amethyst_nutritions',
+            'comment'    => 'Nutrition',
+            'model'      => Railken\Amethyst\Models\Nutrition::class,
+            'schema'     => Railken\Amethyst\Schemas\NutritionSchema::class,
             'repository' => Railken\Amethyst\Repositories\NutritionRepository::class,
-
-            /*
-            |--------------------------------------------------------------------------
-            | Class Serializer
-            |--------------------------------------------------------------------------
-            |
-            | The class of the serializer used to serialize the model.
-            | Change this if you have to add more data while serializing.
-            |
-            */
             'serializer' => Railken\Amethyst\Serializers\NutritionSerializer::class,
-
-            /*
-            |--------------------------------------------------------------------------
-            | Class Validator
-            |--------------------------------------------------------------------------
-            |
-            | The class of the validator used to validate the parameters.
-            | Change this if you have to add more complex validation.
-            | A validation handled by the single attributes is always preferred to this.
-            |
-            */
-            'validator' => Railken\Amethyst\Validators\NutritionValidator::class,
-
-            /*
-            |--------------------------------------------------------------------------
-            | Class Authorizer
-            |--------------------------------------------------------------------------
-            |
-            | The class of the authorizer used to authorize the user.
-            | Change this if you have to add more complex authorization.
-            |
-            */
+            'validator'  => Railken\Amethyst\Validators\NutritionValidator::class,
             'authorizer' => Railken\Amethyst\Authorizers\NutritionAuthorizer::class,
+            'faker'      => Railken\Amethyst\Fakers\NutritionFaker::class,
+            'managers'   => Railken\Amethyst\Managers\NutritionManager::class,
         ],
     ],
 
--- src/Models/Nutrition.php
+++ src/Models/Nutrition.php
@@ -4,8 +4,7 @@ namespace Railken\Amethyst\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Support\Facades\Config;
-use Railken\Amethyst\Schemas\NutritionSchema;
+use Railken\Amethyst\Common\ConfigurableModel;
 use Railken\Lem\Contracts\EntityContract;
 
 /**
@@ -13,17 +12,16 @@ use Railken\Lem\Contracts\EntityContract;
  */
 class Nutrition extends Model implements EntityContract
 {
-    use SoftDeletes;
+    use SoftDeletes, ConfigurableModel;
 
     /**
-     * Creates a new instance of the model.
+     * Create a new Eloquent model instance.
      *
      * @param array $attributes
      */
     public function __construct(array $attributes = [])
     {
+        $this->ini('amethyst.nutrition.data.nutrition');
         parent::__construct($attributes);
-        $this->table = Config::get('amethyst.nutrition.managers.nutrition.table');
-        $this->fillable = (new NutritionSchema())->getNameFillableAttributes();
     }
 }
