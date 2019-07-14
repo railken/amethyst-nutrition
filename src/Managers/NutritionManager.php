6fd2c447dd0bc74bf18990884ef24f522f414926
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/NutritionManager.php
+++ src/Managers/NutritionManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Nutrition newEntity()
+ * @method \Amethyst\Schemas\NutritionSchema getSchema()
+ * @method \Amethyst\Repositories\NutritionRepository getRepository()
+ * @method \Amethyst\Serializers\NutritionSerializer getSerializer()
+ * @method \Amethyst\Validators\NutritionValidator getValidator()
+ * @method \Amethyst\Authorizers\NutritionAuthorizer getAuthorizer()
+ */
 class NutritionManager extends Manager
 {
     use ConfigurableManager;
