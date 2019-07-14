be73f22cbcba6d1e62d732e9503837e229e19c3a
railken <railken@protonmail.ch>
2019-07-14 13:10:16 +0200

    fix style

--- src/Managers/NutritionManager.php
+++ src/Managers/NutritionManager.php
@@ -6,12 +6,12 @@ use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
 /**
- * @method \Amethyst\Models\Nutrition newEntity()
- * @method \Amethyst\Schemas\NutritionSchema getSchema()
+ * @method \Amethyst\Models\Nutrition                 newEntity()
+ * @method \Amethyst\Schemas\NutritionSchema          getSchema()
  * @method \Amethyst\Repositories\NutritionRepository getRepository()
- * @method \Amethyst\Serializers\NutritionSerializer getSerializer()
- * @method \Amethyst\Validators\NutritionValidator getValidator()
- * @method \Amethyst\Authorizers\NutritionAuthorizer getAuthorizer()
+ * @method \Amethyst\Serializers\NutritionSerializer  getSerializer()
+ * @method \Amethyst\Validators\NutritionValidator    getValidator()
+ * @method \Amethyst\Authorizers\NutritionAuthorizer  getAuthorizer()
  */
 class NutritionManager extends Manager
 {
