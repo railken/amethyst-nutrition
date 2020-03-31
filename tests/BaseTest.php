3c81c003d546f69af7d7fe5c8f7d43be92f1b6e6
Railken <railken@protonmail.ch>
2020-04-01 00:24:39 +0200

    DataNameAttribute

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -11,8 +11,6 @@ abstract class BaseTest extends \Orchestra\Testbench\TestCase
     {
         parent::setUp();
 
-        app('amethyst')->pushMorphRelation('nutrition', 'nutritionable', 'foo');
-
         $this->artisan('migrate:fresh');
     }
 
